5030e072c5cd4493fbd2b2423a687afd9f12aae2
Fernando <luizluizmiguel00@gmail.com>
2022-07-07 16:57:23 -0300

    exclusão da descrição do admin

--- 0512-Luiz/0512-luiz/admin/categoria-processo.php
+++ 0512-Luiz/0512-luiz/admin/categoria-processo.php
@@ -17,12 +17,11 @@ switch ($acao) {
         case 'salvar';
         
         $nome = $_POST['nome'];
-        $descricao = $_POST['descricao'];
 
         if( !isset($_POST['id']) || empty($_POST['id'])){
-            $sql = "INSERT INTO `cetegorias`( `Nome`, `Descricao`) VALUES ('".$nome."','".$descricao."')";
+            $sql = "INSERT INTO `cetegorias`( `Nome`) VALUES ('".$nome."')";
         }else{
-            $sql = "UPDATE `categorias` SET `Nome`='".$nome."', `Descricao`='".$descricao."' WHERE `CategoriaID`='".$id."' ";
+            $sql = "UPDATE `categorias` SET `Nome`='".$nome."' WHERE `CategoriaID`='".$id."' ";
         }
 
         mysqli_query($conexao, $sql);
